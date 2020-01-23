8a447fba1c7af162ce5e1eae252839c501d8e0bc
Didiron <60019213+Didiron@users.noreply.github.com>
2020-01-23 21:29:20 +0200

    {d}

---
+++ di-di/1.py
@@ -0,0 +1,20 @@
+import random
+import bn256 
+r1=random.randrange(2, bn256.order)
+r2=random.randrange(2, bn256.order)
+r3=random.randrange(2, bn256.order)
+print('r1 =',r1)
+print('r2 =',r2)
+print('r3 =',r3)
+G1 = bn256.curve_G
+G2 = bn256.twist_G
+print('G1=',G1)
+print('G2=',G2)
+
+T1=G1.scalar_mul(r1)
+T2=G1.scalar_mul(r2)
+T3=G1.scalar_mul(r3)
+print ('T1=',T1)
+print('T2=',T2)
+print('T3=',T3)
+
