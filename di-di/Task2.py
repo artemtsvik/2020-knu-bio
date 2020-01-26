77abf29764c217474d8e4f1e50546681804b514a
Didiron <60019213+Didiron@users.noreply.github.com>
2020-01-26 17:52:20 +0200

    {task2}

--- di-di/Task2.py
+++ di-di/Task2.py
@@ -3,3 +3,13 @@ v1=np.random.choice([1,-1],16 )
 print('v1 =',v1)
 v2=np.random.choice([1,-1],16)
 print('v2 =',v2)
+
+
+def hamming_distance(string1, string2):
+    distance = 0
+    L = len(string1)
+    for i in range(L):
+        if string1[i] != string2[i]:
+            distance += 1
+    return distance
+print(hamming_distance(v1,v2))
