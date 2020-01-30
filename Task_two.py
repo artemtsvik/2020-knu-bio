7739e1b162fd661fb69ee0c1e6fe06c7c9725c00
DimGonchar <honchardm@gmail.com>
2020-01-30 22:15:25 +0200

    start of solution number_3 and change taks_2

--- Task_two.py
+++ Task_two.py
@@ -5,9 +5,9 @@ def hamming_distance(v1, v2):
     if len(v1) == len(v2):
         return 0.5*(len(v1)-np.dot(v1, v2))
 
+if __name__ == '__main__':
+    random_16_vector = (np.random.choice([1, -1], (2, 16)))
+    print(random_16_vector)
 
-random_16_vector = (np.random.choice([1, -1], (2, 16)))
-print(random_16_vector)
 
-
-print(hamming_distance(random_16_vector[0], random_16_vector[1]))
+    print(hamming_distance(random_16_vector[0], random_16_vector[1]))
